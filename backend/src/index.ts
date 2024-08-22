f09a3ee6d052fb53173822ce412508b6f1836410
Christian Tolentino <christiantolentino@192-168-1-125.tpgi.com.au>
2024-08-22 22:17:25 +1000

    Added linter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: { browser: true, es2021: true },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    extends: [
+      'plugin:react/jsx-runtime',
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended',
+    ],
+  },
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: { 'prettier/prettier': 'error' },
+};
